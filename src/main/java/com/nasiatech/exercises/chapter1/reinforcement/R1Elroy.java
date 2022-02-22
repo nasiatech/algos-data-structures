595cd358f239f2f0f1ae659823daf63891d2bdd3
Elroy Kanye <elroykanye@gmail.com>
2022-02-22 17:57:55 +0100

    impl test and code for R1 and R3

--- src/main/java/com/nasiatech/exercises/chapter1/reinforcement/R1Elroy.java
+++ src/main/java/com/nasiatech/exercises/chapter1/reinforcement/R1Elroy.java
@@ -1,7 +1,32 @@
 package com.nasiatech.exercises.chapter1.reinforcement;
 
+import java.util.Scanner;
+
 public class R1Elroy {
+    private final Scanner scanner = new Scanner(System.in);
     public int add(int a, int b) {
         return a + b;
+    }
+
+    public void r1InputAllBaseTypes() {
+        System.out.println("Enter byte");
+        byte byteVal = scanner.nextByte();
+
+        System.out.println("Enter int");
+        int intVal = scanner.nextInt();
+
+        System.out.println("Enter long");
+        long longVal = scanner.nextLong();
+
+        System.out.println("Enter double");
+        double doubleVal = scanner.nextDouble();
+
+        System.out.println("Enter char");
+        char charVal = scanner.nextLine().toCharArray()[0];
+    }
+
+    public boolean r3IsMultiple(long n, long m) {
+        if (m == 0) return false;
+        return n % m == 0;
     }
 }
--- src/test/java/com/nasiatech/exercises/chapter1/reinforcement/R1ElroyTest.java
+++ src/test/java/com/nasiatech/exercises/chapter1/reinforcement/R1ElroyTest.java
@@ -1,7 +1,24 @@
 package com.nasiatech.exercises.chapter1.reinforcement;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class R1ElroyTest {
+    private final R1Elroy elroy = new R1Elroy();
+
+    @Test
+    public void testAdd() {
+
+        assertEquals(5, new R1Elroy().add(2,3));
+    }
+
+    @Test
+    public void testR3() {
+        assertTrue(elroy.r3IsMultiple(6, 3));
+        assertFalse(elroy.r3IsMultiple(5, 2));
+
+        System.out.println(elroy.r3IsMultiple(4,0));
+    }
 
 }
