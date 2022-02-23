3de780f4a8963491c29408d810cde15ba72c4db4
Lambiv Dzenyuy <gillslambiv@gmail.com>
2022-02-23 09:49:13 +0100

    r11 solved

---
+++ src/main/java/com/nasiatech/exercises/chapter1/reinforcement/R1Dzenyuy.java
@@ -0,0 +1,33 @@
+package com.nasiatech.exercises.chapter1.reinforcement;
+
+import java.util.Scanner;
+
+public class R1Dzenyuy {
+    private static Scanner input = new Scanner(System.in);
+
+    public void inputAllBaseTypes(){
+        System.out.print("Enter a string :");
+        String c = input.next();
+        System.out.printf("You entered : %s\n\n", c);
+
+        System.out.print("Enter a long value: ");
+        long l = input.nextLong();
+        System.out.printf("You entered : %d\n\n", l);
+
+        System.out.print("Enter a double :");
+        double d = input.nextDouble();
+        System.out.printf("You entered : %f\n\n", d);
+
+        System.out.print("Enter a float :");
+        float f = input.nextFloat();
+        System.out.printf("You entered : %f\n\n", f);
+
+        System.out.print("Enter an integer : ");
+        int i = input.nextInt();
+        System.out.printf("You entered : %d\n\n", i);
+
+        System.out.print("Enter a character : ");
+        char c1 = input.next().charAt(0);
+        System.out.printf("You entered : %s\n\n", c1);
+    }
+}
