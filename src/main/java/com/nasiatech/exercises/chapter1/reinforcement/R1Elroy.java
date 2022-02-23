c57f8900c083e8c1ff443b88b2eab4bc3aadcec1
Elroy Kanye <elroykanye@gmail.com>
2022-02-23 16:50:03 +0100

    implemented the Flower base class

--- src/main/java/com/nasiatech/exercises/chapter1/reinforcement/R1Elroy.java
+++ src/main/java/com/nasiatech/exercises/chapter1/reinforcement/R1Elroy.java
@@ -87,3 +87,46 @@ public class R1Elroy {
     }
 
 }
+
+class Flower{
+    private String name;
+    private int numOfPetals;
+    private float price;
+
+    public Flower(String name, int numOfPetals, float price) {
+        this.name = name; this.numOfPetals = numOfPetals; this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumOfPetals() {
+        return numOfPetals;
+    }
+
+    public void setNumOfPetals(int numOfPetals) {
+        this.numOfPetals = numOfPetals;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
+
+class R1ElroyUtil {
+    public static boolean arrContains(char[] arr, char item) {
+        for (char i: arr) {
+            if(i == item) return true;
+        } return false;
+    }
+}
+
